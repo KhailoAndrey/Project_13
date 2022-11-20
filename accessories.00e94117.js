c594f769dedc4b57e4dee0d5e2d3e3ac91e72f7e
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2022-11-20 15:22:35 +0000

    Deploying to gh-pages from @ KhailoAndrey/Project_13@57c80d1f194e582f9c1ca8eef23b22282d011fd8 🚀

---
+++ accessories.00e94117.js
@@ -0,0 +1,2 @@
+new Swiper(".banner__slider",{loop:!0,initialSlide:0,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:1e3,stopOnLastSlide:!0,disableOnInteraction:!1},speed:3e3,effect:"fade",crossFade:!0});
+//# sourceMappingURL=accessories.00e94117.js.map
