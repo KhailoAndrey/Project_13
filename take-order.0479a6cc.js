7047c8bd5d051da31bb66af047d65b47c72f2eeb
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2022-11-21 18:45:28 +0000

    Deploying to gh-pages from @ KhailoAndrey/Project_13@219f6d2c76488a6d178d1d3cbc2ef92bccc0d69b 🚀

--- take-order.0479a6cc.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=take-order.0479a6cc.js.map
--- take-order.936f05b0.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
-//# sourceMappingURL=take-order.936f05b0.js.map
