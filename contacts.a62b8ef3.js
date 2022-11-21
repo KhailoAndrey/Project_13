d3aaadaa7684fc21324e24c7a59b91645c3926b1
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2022-11-21 22:28:13 +0000

    Deploying to gh-pages from @ KhailoAndrey/Project_13@4bfcfa700670b9ab6bcc2f6c9229336b79b32e80 🚀

--- contacts.a62b8ef3.js
+++ contacts.a62b8ef3.js
@@ -1,2 +1,2 @@
 !function(){var t={openContactBtn:document.querySelector("[data-modal-open]"),closeContactBtn:document.querySelector("[data-modal-close]"),contact:document.querySelector("[data-modal]")};function e(){t.contact.classList.toggle("is-hidden")}t.openContactBtn.addEventListener("click",e),t.closeContactBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.e2c4ce0c.js.map
+//# sourceMappingURL=contacts.a62b8ef3.js.map
--- contacts.fdad797d.js
+++ contacts.fdad797d.js
@@ -1,2 +1,2 @@
 (()=>{const t={openContactBtn:document.querySelector("[data-modal-open]"),closeContactBtn:document.querySelector("[data-modal-close]"),contact:document.querySelector("[data-modal]")};function e(){t.contact.classList.toggle("is-hidden")}t.openContactBtn.addEventListener("click",e),t.closeContactBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.a2135039.js.map
+//# sourceMappingURL=contacts.fdad797d.js.map
