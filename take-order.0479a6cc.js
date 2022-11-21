472dc4d07f7e8229fdf89a6b709ff31af7c6a53a
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2022-11-21 01:50:04 +0000

    Deploying to gh-pages from @ KhailoAndrey/Project_13@74fe95773e9c44db6f98567145dee07ac694ef58 🚀

--- take-order.0479a6cc.js
+++ take-order.0479a6cc.js
@@ -1,2 +1,2 @@
 !function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=blog-article.0801dc91.js.map
+//# sourceMappingURL=take-order.0479a6cc.js.map
--- take-order.936f05b0.js
+++ take-order.936f05b0.js
@@ -1,2 +1,2 @@
 (()=>{const e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
-//# sourceMappingURL=blog-article.277483f7.js.map
+//# sourceMappingURL=take-order.936f05b0.js.map
