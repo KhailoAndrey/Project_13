3c154331888f98fa503dc70eb45ef749ecd6c5fb
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2023-02-25 13:26:50 +0000

    Deploying to gh-pages from @ KhailoAndrey/Project_13@bb1fc810acb983979376ae172742729175cc63f1 🚀

---
+++ contacts.fdad797d.js
@@ -0,0 +1,2 @@
+(()=>{const t={openContactBtn:document.querySelector("[data-modal-open]"),closeContactBtn:document.querySelector("[data-modal-close]"),contact:document.querySelector("[data-modal]")};function e(){t.contact.classList.toggle("is-hidden")}t.openContactBtn.addEventListener("click",e),t.closeContactBtn.addEventListener("click",e)})();
+//# sourceMappingURL=contacts.fdad797d.js.map
